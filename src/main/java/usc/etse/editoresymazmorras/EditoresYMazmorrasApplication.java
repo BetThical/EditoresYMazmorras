8da691d91cdf97688ff43b4c988dafbfa76bf513
XabiSuarez <xabisc777@gmail.com>
2026-09-23 10:57:57 +0200

    Estructura de carpetas y ficheros básica.

---
+++ src/main/java/usc/etse/editoresymazmorras/EditoresYMazmorrasApplication.java
@@ -0,0 +1,13 @@
+package usc.etse.editoresymazmorras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditoresYMazmorrasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EditoresYMazmorrasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/usc/etse/editoresymazmorras/EditoresYMazmorrasApplicationTests.java
@@ -0,0 +1,13 @@
+package usc.etse.editoresymazmorras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditoresYMazmorrasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
